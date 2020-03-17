af135793ade1c86ca6136905d218596950dc7a08
leon.zhou <leon.zhou@vendhq.com>
2020-03-17 21:45:42 +1300

    update travis

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ extra["springCloudVersion"] = "Hoxton.SR3"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation('org.springframework.cloud:spring-cloud-config-server')
+    implementation("org.springframework.cloud:spring-cloud-config-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
